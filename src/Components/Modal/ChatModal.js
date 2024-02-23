b7660d61c72e9c9c5b1212d7d2a03663d4072e0f
jonghechoi <sosinnmi@naver.com>
2024-02-23 14:08:12 +0900

    feat(Chat Front): 채팅 모달창 작업
    각 Board 별 채팅방 MongoDB 저장 테스트 진행
    
    최종희

--- src/Components/Modal/ChatModal.js
+++ src/Components/Modal/ChatModal.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
+import Stomp from "stompjs";
+import SockJS from "sockjs-client";
 import '../../css/ChatModal.css';
 
 const ChatModal = ({ chatModalName, onClose }) => {
@@ -6,75 +8,111 @@ const ChatModal = ({ chatModalName, onClose }) => {
     const [message, setMessage] = useState('');
     const [sendMsg, setSendMsg] = useState(false);
     const [items, setItems] = useState([]);
-    const webSocketUrl = "ws://localhost:8083/api/chat";
-    let ws = useRef(null);
-
+    const [stompClient, setStompClient] = useState(null);
+    const webSocketUrl = "http://localhost:8083/chat/pub";  // 웹소켓 엔드포인트
     const [boardName, setBoardName] = useState('');
 
     useEffect(() => {
-        if (chatModalName)
+        if (chatModalName) {
             setBoardName(chatModalName);
+        }
+
+        const socket = new SockJS(webSocketUrl);
+        const stomp = Stomp.over(socket);
+
+        stomp.connect({}, () => {
+            console.log('Stomp 연결 성공');
+            setSocketConnected(true);
+
+            console.log(`이거 맞지?? + ${chatModalName}`);
+
+            stomp.subscribe(`/topic/${chatModalName}`, (message) => {
+                // stomp.subscribe(`/topic/aaa`, (message) => {
+                const data = JSON.parse(message.body);
+                console.log(data.message);
+                setItems((prevItems) => [...prevItems, data.message]);
+            });
+        });
+
+        setStompClient(stomp);
+
+        return () => {
+            // 컴포넌트가 언마운트 될 때 Stomp 연결 해제
+            stomp.disconnect();
+        };
     }, [chatModalName]);
 
-    const handleKeyDown = (e) => {
-        if(e.key === 'Enter') {
+    // useEffect(() => {
+    //     const socket = new SockJS(webSocketUrl);
+    //     const stomp = Stomp.over(socket);
+    //
+    //     stomp.connect({}, () => {
+    //         console.log('Stomp 연결 성공');
+    //         setSocketConnected(true);
+    //
+    //         stomp.subscribe(`/topic/${boardName}`, (message) => {
+    //         // stomp.subscribe(`/topic/aaa`, (message) => {
+    //             const data = JSON.parse(message.body);
+    //             console.log(data.message);
+    //             setItems((prevItems) => [...prevItems, data.message]);
+    //         });
+    //     });
+    //
+    //     setStompClient(stomp);
+    //
+    //     return () => {
+    //         // 컴포넌트가 언마운트 될 때 Stomp 연결 해제
+    //         stomp.disconnect();
+    //     };
+    // }, [boardName, webSocketUrl]);
 
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            if (socketConnected) {
+                stompClient.send(`/chat/pub/topic/${boardName}`, {},
+                    JSON.stringify(
+                        {
+                                type: "TALK",
+                                boardId: boardName,
+                                sender: "?",
+                                message: message,
+                                time: Date.now()
+                            }
+                    )
+                );
+                setMessage('');
+                setSendMsg(true);
+            }
         }
     };
 
-    // useEffect(() => {
-    //     ws = new WebSocket(webSocketUrl);
-    //     ws.current.onopen = () => {
-    //         console.log('WebSocket 연결');
-    //         setSocketConnected(true);
-    //     }
-    //     ws.current.onclose = (error) => {
-    //         console.log('WebSocket 연결 끊김');
-    //     }
-    //     ws.current.onerror = (error) => {
-    //         console.log(error);
-    //     }
-    //     ws.current.onmessage = (e) => {
-    //         const data = JSON.parse(e.data);
-    //         console.log(data);
-    //         setItems((prevItems) => [...prevItems, data]);
-    //     }
-    // }, []);
-    //
-    // useEffect(() => {
-    //     if(socketConnected) {
-    //         ws.current.send(
-    //             JSON.stringify({
-    //                 message: sendMessage
-    //             })
-    //         )
-    //         setSendMsg(true);
-    //     }
-    // }, [socketConnected]);
-    //
-    // useEffect(() => {
-    //     if (sendMsg) {
-    //         ws.current.onmessage = (e) => {
-    //             const data = JSON.parse(e.data);
-    //             console.log(data);
-    //             setItems((prevItems) => [...prevItems, data]);
-    //         };
-    //     }
-    // }, [sendMsg]);
+    useEffect(() => {
+        if (sendMsg) {
+            stompClient.onmessage = (e) => {
+                const data = JSON.parse(e.body);
+                console.log(data);
+                setItems((prevItems) => [...prevItems, data]);
+            };
+        }
+    }, [sendMsg]);
+    // }, [sendMsg, stompClient]);
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-container" onClick={(e) => e.stopPropagation()}>
                 <div className="modal-content">
-                    {items.map((item) => {
-                        return <div>{ JSON.stringify(item) } </div>
-                    })}
+                    {items.map((item, index) => (
+                        <div key={index}>{
+
+                                item
+
+                        }</div>
+                    ))}
                     <input
                         type="text"
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
                         onKeyDown={handleKeyDown}
-                        // placeholder={"대화창"}
                         placeholder={boardName}
                     />
                 </div>
