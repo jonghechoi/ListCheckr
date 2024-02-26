373dd1b286fc159d0dbd1ac9ccd4ac3fb0e92c3f
jonghechoi <sosinnmi@naver.com>
2024-02-26 20:32:05 +0900

    feat(Chat Front): 채팅 모달창 작업 완료
    각 유저&Board 별 채팅창 구현 완료
    
    최종희

--- src/Components/Modal/ChatModal.js
+++ src/Components/Modal/ChatModal.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Stomp from "stompjs";
 import SockJS from "sockjs-client";
 import '../../css/ChatModal.css';
+import axios from "axios";
 
 const ChatModal = ({ chatModalName, onClose }) => {
     const [socketConnected, setSocketConnected] = useState(false);
@@ -9,7 +10,8 @@ const ChatModal = ({ chatModalName, onClose }) => {
     const [sendMsg, setSendMsg] = useState(false);
     const [items, setItems] = useState([]);
     const [stompClient, setStompClient] = useState(null);
-    const webSocketUrl = "http://localhost:8083/chat/pub";  // 웹소켓 엔드포인트
+    const webSocketUrl = "http://localhost:8083/chat/pub";
+    const apiInstance = axios.create({ baseURL: "http://localhost:8083/api/chat/" });
     const [boardName, setBoardName] = useState('');
 
     useEffect(() => {
@@ -20,65 +22,59 @@ const ChatModal = ({ chatModalName, onClose }) => {
         const socket = new SockJS(webSocketUrl);
         const stomp = Stomp.over(socket);
 
-        stomp.connect({}, () => {
+        stomp.connect({ boardId : chatModalName, sender : "초기 생성인", createTime :  Date.now() }, () => {
             console.log('Stomp 연결 성공');
             setSocketConnected(true);
 
-            console.log(`이거 맞지?? + ${chatModalName}`);
-
             stomp.subscribe(`/topic/${chatModalName}`, (message) => {
-                // stomp.subscribe(`/topic/aaa`, (message) => {
                 const data = JSON.parse(message.body);
-                console.log(data.message);
-                setItems((prevItems) => [...prevItems, data.message]);
+                setItems((prevItems) => [...prevItems, data.messageContent]);
             });
         });
+
+        fetchChatHistory();
 
         setStompClient(stomp);
 
         return () => {
-            // 컴포넌트가 언마운트 될 때 Stomp 연결 해제
-            stomp.disconnect();
+            let disconnectHeaders = {
+                boardId: chatModalName,
+            };
+            stomp.disconnect(0, disconnectHeaders);
         };
     }, [chatModalName]);
 
-    // useEffect(() => {
-    //     const socket = new SockJS(webSocketUrl);
-    //     const stomp = Stomp.over(socket);
-    //
-    //     stomp.connect({}, () => {
-    //         console.log('Stomp 연결 성공');
-    //         setSocketConnected(true);
-    //
-    //         stomp.subscribe(`/topic/${boardName}`, (message) => {
-    //         // stomp.subscribe(`/topic/aaa`, (message) => {
-    //             const data = JSON.parse(message.body);
-    //             console.log(data.message);
-    //             setItems((prevItems) => [...prevItems, data.message]);
-    //         });
-    //     });
-    //
-    //     setStompClient(stomp);
-    //
-    //     return () => {
-    //         // 컴포넌트가 언마운트 될 때 Stomp 연결 해제
-    //         stomp.disconnect();
-    //     };
-    // }, [boardName, webSocketUrl]);
+    const fetchChatHistory = async () => {
+        try {
+            const response = await apiInstance.get(chatModalName);
+            let newItems = response.data.messageContentList.map((content) => (
+                {
+                    sender: content.sender,
+                    content: content.content,
+                    sendTime: content.sendTime,
+                }
+            ));
+
+            setItems((prevItems) => [...prevItems, ...newItems]);
+        } catch (error) {
+            console.error('채팅 목록 가져오기 중 에러 발생: ', error)
+        }
+    }
 
     const handleKeyDown = (e) => {
         if (e.key === 'Enter') {
             if (socketConnected) {
-                stompClient.send(`/chat/pub/topic/${boardName}`, {},
-                    JSON.stringify(
-                        {
-                                type: "TALK",
-                                boardId: boardName,
-                                sender: "?",
-                                message: message,
-                                time: Date.now()
-                            }
-                    )
+                stompClient.send(`/chat/pub/topic/${chatModalName}`, { boardId : chatModalName },
+                    JSON.stringify({
+                            boardId: chatModalName,
+                            messageContent: {
+                                // 추후 로그인 ID로 변경 필수
+                                sender: "b",
+                                content: message,
+                                sendTime: Date.now()
+                            },
+                            type: "TALK"
+                    })
                 );
                 setMessage('');
                 setSendMsg(true);
@@ -90,32 +86,31 @@ const ChatModal = ({ chatModalName, onClose }) => {
         if (sendMsg) {
             stompClient.onmessage = (e) => {
                 const data = JSON.parse(e.body);
-                console.log(data);
                 setItems((prevItems) => [...prevItems, data]);
             };
         }
     }, [sendMsg]);
-    // }, [sendMsg, stompClient]);
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-container" onClick={(e) => e.stopPropagation()}>
                 <div className="modal-content">
                     {items.map((item, index) => (
-                        <div key={index}>{
-
-                                item
-
-                        }</div>
+                        <p
+                            key={index}
+                            className={item.sender === 'b' ? 'right-align' : 'left-align'}
+                        >
+                            Sender: {item.sender} Content: {item.content}
+                        </p>
                     ))}
-                    <input
-                        type="text"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                        placeholder={boardName}
-                    />
                 </div>
+                <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    placeholder={boardName}
+                />
                 <button onClick={onClose}>Close</button>
             </div>
         </div>
